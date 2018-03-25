13d3cb8bb60d5bb1863aa43371b44ae20ababf0a
Rais <raishab@yandex.ru>
2018-03-25 19:07:10 +0400

    changed way of accepting data

--- demo-web/hello.py
+++ demo-web/hello.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-@app.route('/uploader', methods=['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save('uploaded_file')
-      
-      return 'success'
-
-@app.route('/results')
-def results():
-    return
+      f1 = request.form['text1']
+      f2 = request.form['text2']
+      if f1 == f2:
+          whether_dup = u'являются'
+      else:
+          whether_dup = u'не являются'
+   return render_template('results.html', whether_dup = whether_dup)
 
 if __name__ == '__main__':
     app.run(debug=True)
